79b0639488b1813501abe3e9c34f3c435d9ff705
Shankara21 <lazuardit21@gmail.com>
2022-03-14 13:22:12 +0700

    Menambahkan model pegawai, barang, pelanggan, dan supplier

---
+++ app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pegawai;
+use App\Http\Requests\StorePegawaiRequest;
+use App\Http\Requests\UpdatePegawaiRequest;
+
+class PegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePegawaiRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePegawaiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePegawaiRequest  $request
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePegawaiRequest $request, Pegawai $pegawai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pegawai  $pegawai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pegawai $pegawai)
+    {
+        //
+    }
+}
