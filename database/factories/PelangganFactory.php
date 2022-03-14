53b7d1dafd4760eb4138bd400121cc5f138a74e4
Firgisotya <zzuuddiinn28@gmail.com>
2022-03-14 22:25:31 +0700

    membuat factory category dan pelanggan

--- database/factories/PelangganFactory.php
+++ database/factories/PelangganFactory.php
@@ -14,7 +14,11 @@ class PelangganFactory extends Factory
     public function definition()
     {
         return [
-            //
+                'nama_pelanggan' => $this->faker->name,
+                'alamat' => $this->faker->address,
+                'telepon' => $this->faker->phoneNumber,
+                'jenis_kelamin' => $this->faker->randomElement(['Laki-laki', 'Perempuan']),
+
         ];
     }
 }
