4da685bf2d035be5c07bec88f474329abc0f1113
Firgisotya <zzuuddiinn28@gmail.com>
2022-03-14 22:24:13 +0700

    mengubah model barang,category,pelanggan

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $with = ['category'];
+
+    public function scopeFilter($query, array $filters){
+
+        $query->when($filters['category'] ?? false, function($query, $category){
+            return $query->whereHas('category', function($query) use ($category){
+                $query->where('slug', $category);
+            });
+        });
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Pelanggan extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function barangs(){
+        return $this->hasMany(Barang::class);
+    }
 }
