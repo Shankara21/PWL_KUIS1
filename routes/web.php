9dccdade9a2944ce35f5b83f4ca288320b57247e
Shankara21 <lazuardit21@gmail.com>
2022-03-14 14:59:57 +0700

    Menata Layouts

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/shop', function () {
+    return view('shop');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
