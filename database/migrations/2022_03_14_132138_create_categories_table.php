df9dfad3db37ac0cd42474f6ca0819016aec078c
Firgisotya <zzuuddiinn28@gmail.com>
2022-03-14 22:25:56 +0700

    mengubah migration barang dan category

--- database/migrations/2022_03_14_132138_create_categories_table.php
+++ database/migrations/2022_03_14_132138_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBarangsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateBarangsTable extends Migration
      */
     public function up()
     {
-        Schema::create('barangs', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('category_id');
             $table->string('name');
             $table->string('slug');
-            $table->string('size');
-            $table->integer('price');
             $table->string('image')->nullable();
             $table->timestamps();
         });
@@ -32,6 +29,6 @@ class CreateBarangsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('barangs');
+        Schema::dropIfExists('categories');
     }
 }
